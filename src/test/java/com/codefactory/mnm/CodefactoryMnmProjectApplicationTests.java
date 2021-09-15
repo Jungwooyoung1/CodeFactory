00bbcb7a072b001b58f89b8945b432a93daab8a9
wooyoungjung3379@gmail.com <wooyoungjung3379@gmail.com>
2021-09-15 18:38:23 +0900

    project init

---
+++ src/test/java/com/codefactory/mnm/CodefactoryMnmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefactory.mnm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodefactoryMnmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
